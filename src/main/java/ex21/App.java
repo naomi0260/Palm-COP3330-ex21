565ae26a773479a5ba575a566bce17a3dacb1df6
naomi0260 <84354219+naomi0260@users.noreply.github.com>
2021-06-07 16:40:41 -0400

    Update App.java

--- src/main/java/ex21/App.java
+++ src/main/java/ex21/App.java
@@ -3,14 +3,8 @@ package ex21;
 import java.util.Scanner;
 
 /*
-Write a program that converts a number from 1 to 12 to the corresponding month. Prompt for a number and display the corresponding calendar month, with 1 being January and 12 being December. For any value outside that range, display an appropriate error message.
-
-Example Output
-Please enter the number of the month: 3
-The name of the month is March.
-Constraints
-Use a switch or case statement for this program.
-Use a single output statement for this program.
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Naomi Palm
  */
 
 public class App {
